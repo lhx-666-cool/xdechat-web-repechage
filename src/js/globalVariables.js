011a114d76ffbc31ba993c467aea54810ef65164
lhx-666-cool <d3032471065@yeah.net>
2025-04-13 19:53:35 +0800

    test

--- src/js/globalVariables.js
+++ src/js/globalVariables.js
@@ -1,6 +1,6 @@
-const backendUrl = "https://xdechat.xidian.edu.cn/formatapi"
+// const backendUrl = "https://xdechat.xidian.edu.cn/formatapi"
 // const backendUrl = "http://liu.steesha.cn:2222"
-// const backendUrl = "http://127.0.0.1:2222"
+const backendUrl = "http://127.0.0.1:2222"
 
 export {
     backendUrl
